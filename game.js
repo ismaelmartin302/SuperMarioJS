0351a76f05df77641dd436b34c98e6b7e3c6ef30
isma <ismaelmartin302@gmail.com>
2024-07-17 20:39:21 +0200

    la primera animacion funciona

--- game.js
+++ game.js
@@ -27,18 +27,33 @@ function preload() {
         'assets/entities/mario.png',
         { frameWidth: 18, frameHeight: 16 }
     )
+
 } // Paso 1
 function create() {
     this.add.image(0, 0, 'cloud1').setScale(.15).setOrigin(0, 0);
     this.mario = this.add.sprite(50, 210, 'mario').setOrigin(0, 1);
     this.add.tileSprite(0, config.height - 32, config.width, 32, 'floorbricks').setOrigin(0, 0);;
     this.keys = this.input.keyboard.createCursorKeys()
+
+    this.anims.create({
+        key: 'mario-walk',
+        frames: this.anims.generateFrameNumbers(
+            'mario',
+            { start: 1, end: 3 },
+        ),
+        frameRate: 12,
+        repeat: -1
+    })
 } // Paso 2
 function update() {
     if (this.keys.left.isDown) {
+        this.mario.anims.play('mario-walk', true)
         this.mario.x -= 2
-    }
-    if (this.keys.right.isDown) {
+    } else if (this.keys.right.isDown) {
+        this.mario.anims.play('mario-walk', true)
         this.mario.x += 2
+    } else {
+        this.mario.anims.stop()
+        this.mario.setFrame(0)
     }
 } // Paso 3...Infinito
